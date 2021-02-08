3b67a931e52fd50f3b3d6a54a6fe3668a359fcdd
Mateusz Szewc <mszewc@lbpro.pl>
2021-02-08 21:36:34 +0100

    basic shitty hibernate setup

--- src/main/java/sitammatt/example_rest/TaskResource.java
+++ src/main/java/sitammatt/example_rest/TaskResource.java
@@ -1,17 +1,29 @@
 package sitammatt.example_rest;
 
+import sitammatt.example_rest.model.Person;
 import sitammatt.example_rest.model.Task;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/tasks")
 public class TaskResource {
+//    @PersistenceContext
+//    private EntityManager entityManager;
+
     @GET
     public Response get(){
         var task = new Task();
         task.setTitle("Hello Task");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Books");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        em.createNamedQuery("Person.getAll", Person.class).getResultList();
+
         return Response.ok(task).build();
     }
 }
